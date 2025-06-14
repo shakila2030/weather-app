656d648473af92e0f4ecac7f41e95ccd1131ffe9
Shakila Kularathna <105419083+shakila2030@users.noreply.github.com>
2025-06-14 12:13:33 +0530

    Created backend application

---
+++ weather-app-backend/src/main/java/com/example/weather_app_backend/WeatherAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.weather_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WeatherAppBackendApplication.class, args);
+	}
+
+}
