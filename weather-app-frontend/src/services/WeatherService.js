d9d51eb6903b95bfe25b2fe7bec37724408772aa
shakila2030 <105419083+shakila2030@users.noreply.github.com>
2025-06-16 01:51:42 +0530

    Made changes to api base url on frontend

--- weather-app-frontend/src/services/WeatherService.js
+++ weather-app-frontend/src/services/WeatherService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://weather-app-production-4860.up.railway.app";
+const API_BASE_URL = "https://weather-app-production-4860.up.railway.app/api/weather";
 
 export const getWeatherForCity = async city => {
   const response = await axios.get(`${API_BASE_URL}/${city}`);
